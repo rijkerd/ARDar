e18b866b82c69fda6eae0c2da1bb347fbd3b6cf1
rijkerd <richardaggrey7@gmail.com>
2019-02-23 16:24:36 +0300

    Initial Bootstrap

---
+++ app/src/main/java/com/example/ar_dar/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ar_dar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

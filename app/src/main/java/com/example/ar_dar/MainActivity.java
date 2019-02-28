f987e05084b5bd796e66addcfa4964e41a5fb527
rijkerd <richardaggrey7@gmail.com>
2019-02-28 17:41:44 +0300

    Enable ArCore - Add installation of arcore - Fix imports - Fix Camera handle

--- app/src/main/java/com/example/ar_dar/MainActivity.java
+++ app/src/main/java/com/example/ar_dar/MainActivity.java
@@ -3,13 +3,18 @@ package com.example.ar_dar;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.ViewStructure;
+import android.widget.Toast;
 
 import com.google.ar.core.ArCoreApk;
 import com.example.ar_dar.CameraPermissionHelper;
+import com.google.ar.core.Session;
+import com.google.ar.core.exceptions.UnavailableUserDeclinedInstallationException;
 
 public class MainActivity extends AppCompatActivity {
+
+    Session mSession;
+
+    private boolean mUserRequestedInstall = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +31,43 @@ public class MainActivity extends AppCompatActivity {
         if (!CameraPermissionHelper.hasCameraPermission(this)) {
             CameraPermissionHelper.requestCameraPermission(this);
             return;
+        }
+        // Make sure ARCore is installed and up to date.
+        try {
+            if (mSession == null) {
+                switch (ArCoreApk.getInstance().requestInstall(this, mUserRequestedInstall)) {
+                    case INSTALLED:
+                        // Success, create the AR session.
+                        mSession = new Session(this);
+                        break;
+                    case INSTALL_REQUESTED:
+                        // Ensures next invocation of requestInstall() will either return
+                        // INSTALLED or throw an exception.
+                        mUserRequestedInstall = false;
+//                        return ;
+                }
+            }
+        } catch (UnavailableUserDeclinedInstallationException e) {
+            // Display an appropriate message to the user and return gracefully.
+            Toast.makeText(this, "TODO: handle exception " + e, Toast.LENGTH_LONG)
+                    .show();
+//            return;
+        } catch (Exception e) {  // Current catch statements.
+//            return;
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] results) {
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+            Toast.makeText(this, "Camera permission is needed to run this application", Toast.LENGTH_LONG)
+                    .show();
+            if (!CameraPermissionHelper.shouldShowRequestPermissionRationale(this)) {
+                // Permission denied with checking "Do not ask again".
+                CameraPermissionHelper.launchPermissionSettings(this);
+            }
+            finish();
         }
     }
 

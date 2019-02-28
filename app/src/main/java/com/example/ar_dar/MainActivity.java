f2ae7081b886985b6c498d763b8c584d60fd1ae3
rijkerd <richardaggrey7@gmail.com>
2019-02-28 10:17:04 +0300

    Add Camera Permission - Add CameraPermissionHelper class

--- app/src/main/java/com/example/ar_dar/MainActivity.java
+++ app/src/main/java/com/example/ar_dar/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.ar_dar;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewStructure;
+
+import com.google.ar.core.ArCoreApk;
+import com.example.ar_dar.CameraPermissionHelper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +16,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // ARCore requires camera permission to operate.
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+            CameraPermissionHelper.requestCameraPermission(this);
+            return;
+        }
+    }
+
+
 }
